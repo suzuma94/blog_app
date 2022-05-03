5abedacd760d787a31fb46687eea0dba4375074c
Aleksa <alexa.milenkovic1@gmail.com>
2022-05-03 13:23:31 +0200

    v.1.0

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+    has_many :comments, dependent: :destroy
+
+    validates :title, presence: true
+    validates :body, presence: true, length: {minimum: 10}
+
+    include Visible
+
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :article
+  
+
+  include Visible
+
+end
